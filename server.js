92898ea308d3ea487d179f299f80fe1053a3d4c8
PatrickARatcliff <pratcliff5@gmail.com>
2022-09-25 17:23:32 -0600

    add seeds, add inquirer start  question

--- server.js
+++ server.js
@@ -1,6 +1,9 @@
 const express = require('express');
 const mysql = require('mysql2');
 const inquirer = require('inquirer');
+const Employee = require('./lib/Employee');
+const Dept = require('./lib/Dept');
+const Role = require('./lib/Role');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -18,99 +21,210 @@ const db = mysql.createConnection(
   console.log(`Connected to the employee_db database.`)
 );
 
-app.post('/api/new-employee', ({ body }, res) => {
-  const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
-    VALUES (?)`;
-  const params = [body.movie_name];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-      return;
+//inquirer opening question
+startQuestion = () => {
+  return inquirer.prompt([
+    {
+      type: 'list',
+      message: "What would you like to do?",
+      name: 'choice',
+      choices: ["View All Employees", "Add Employee", "Update Employee Role", "View All Roles", "Add Role", "View All Departments", "Add Department", "Quit", "View Company Roster"],
+      loop: false,
     }
-    res.json({
-      message: 'success',
-      data: body
-    });
-  });
-});
+  ])
+    .then(choice => {
+      console.log(choice.choice)
+      switch (choice.choice) {
+        case "View All Employees":
+          viewEmployees();
+          break;
+        case "Add Employee":
+          addEmployee();
+          break;
+        case "Update Employee Role":
+          updateEmployee();
+          break;
+        case "View All Roles":
+          viewRoles();
+          break;
+        case "Add Role":
+          addRole();
+          break;
+        case "View All Departments":
+          viewDepartments();
+          break;
+        case "Add Department":
+          addDepartment();
+          break;
+        case "Quit":
+          quit();
+          break;
+        case "View Company Roster":
+          viewRoster();
+          break;
+      }
+    })
+};
 
-// Read all movies
-app.get('/api/employees', (req, res) => {
-  const sql = `SELECT id, movie_name AS title FROM movies`;
-  
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-       return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
+// Read all employees
+const viewEmployees = () => {
+  app.get('/api/employees', (req, res) => {
+    const sql = `SELECT * FROM employee`;
 
-// Delete a movie
-app.delete('/api/movie/:id', (req, res) => {
-  const sql = `DELETE FROM movies WHERE id = ?`;
-  const params = [req.params.id];
-  
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.statusMessage(400).json({ error: res.message });
-    } else if (!result.affectedRows) {
-      res.json({
-      message: 'Movie not found'
-      });
-    } else {
-      res.json({
-        message: 'deleted',
-        changes: result.affectedRows,
-        id: req.params.id
-      });
-    }
-  });
-});
-
-// Read list of all reviews and associated movie name using LEFT JOIN
-app.get('/api/movie-reviews', (req, res) => {
-  const sql = `SELECT movies.movie_name AS movie, reviews.review FROM reviews LEFT JOIN movies ON reviews.movie_id = movies.id ORDER BY movies.movie_name;`;
-  db.query(sql, (err, rows) => {
-    if (err) {
-      res.status(500).json({ error: err.message });
-      return;
-    }
-    res.json({
-      message: 'success',
-      data: rows
-    });
-  });
-});
-
-// BONUS: Update review name
-app.put('/api/review/:id', (req, res) => {
-  const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
-  const params = [req.body.review, req.params.id];
-
-  db.query(sql, params, (err, result) => {
-    if (err) {
-      res.status(400).json({ error: err.message });
-    } else if (!result.affectedRows) {
-      res.json({
-        message: 'Movie not found'
-      });
-    } else {
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
       res.json({
         message: 'success',
-        data: req.body,
-        changes: result.affectedRows
+        data: rows
       });
-    }
+    })
+  })
+};
+// Add Employee
+const addEmployee = () => {
+  app.post('/api/new-employee', ({ body }, res) => {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+    VALUES (?)`;
+    const params = [body.movie_name];
+
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      })
+    })
+  })
+};
+// Update employee
+const updateEmployee = () => {
+  app.put('/api/employee/:id', (req, res) => {
+    const sql = `UPDATE reviews SET review = ? WHERE id = ?`;
+    const params = [req.body.review, req.params.id];
+
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+      } else if (!result.affectedRows) {
+        res.json({
+          message: 'Movie not found'
+        });
+      } else {
+        res.json({
+          message: 'success',
+          data: req.body,
+          changes: result.affectedRows
+        })
+      }
+    })
+  })
+};
+// Read all roles
+const viewRoles = () => {
+  app.get('/api/roles', (req, res) => {
+    const sql = `SELECT id, movie_name AS title FROM movies`;
+
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      })
+    })
+  })
+};
+// Add role
+const addRole = () => {
+  app.post('/api/new-employee', ({ body }, res) => {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+      VALUES (?)`;
+    const params = [body.movie_name];
+
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
   });
-});
+};
+// Read all roles
+const viewDepartments = () => {
+  app.get('/api/roles', (req, res) => {
+    const sql = `SELECT id, movie_name AS title FROM movies`;
 
+    db.query(sql, (err, rows) => {
+      if (err) {
+        res.status(500).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: rows
+      })
+    })
+  })
+};
+// Add role
+const addDepartment = () => {
+  app.post('/api/new-employee', ({ body }, res) => {
+    const sql = `INSERT INTO employee (first_name, last_name, role_id, manager_id)
+        VALUES (?)`;
+    const params = [body.movie_name];
 
+    db.query(sql, params, (err, result) => {
+      if (err) {
+        res.status(400).json({ error: err.message });
+        return;
+      }
+      res.json({
+        message: 'success',
+        data: body
+      });
+    });
+  });
+};
+// Read list of all employees and associated department and roles using LEFT JOIN
+const viewRoster = () => {
+app.get('/api/employee-roster', (req, res) => {
+  const sql = 
+  `SELECT 
+  employee.id
+  employee.first_name AS first, 
+  employee.last_name AS last, 
+  employee_role.title FROM employee_role AS role, 
+  department.department_name FROM department AS dept, 
+  employee_role.salary FROM employee_role AS salary,
+
+  LEFT JOIN employee_role ON employee.role_id = employee_role.id,
+  LEFT JOIN department ON employee_role.department_id = department.id,
+  
+  ORDER BY employee.id;`;
+  db.query(sql, (err, rows) => {
+    if (err) {
+      res.status(500).json({ error: err.message });
+      return;
+    }
+    res.json({
+      message: 'success',
+      data: rows
+    })
+  })
+})
+};
 
 
 app.use((req, res) => {
