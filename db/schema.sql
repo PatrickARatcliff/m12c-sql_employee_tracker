307bb5b23bffeb10377666354cbaa5a7993705a4
PatrickARatcliff <pratcliff5@gmail.com>
2022-09-25 19:44:56 -0600

    fix sql syntax error in schema

--- db/schema.sql
+++ db/schema.sql
@@ -3,15 +3,10 @@ CREATE DATABASE employee_db;
 
 USE employee_db;
 
-CREATE TABLE employee (
+CREATE TABLE department (
   id INT NOT NULL AUTO_INCREMENT,
-  first_name VARCHAR(30) NOT NULL,
-  last_name VARCHAR(30) NOT NULL,
-  role_id INT NOT NULL,
-  manager_id INT,
-  PRIMARY KEY (id),
-  FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE CASCADE,
-  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+  department_name VARCHAR(30) NOT NULL,
+  PRIMARY KEY (id)
 );
 
 CREATE TABLE employee_role (
@@ -23,8 +18,13 @@ CREATE TABLE employee_role (
   FOREIGN KEY (department_id) REFERENCES department(id) ON DELETE CASCADE
 );
 
-CREATE TABLE department (
+CREATE TABLE employee (
   id INT NOT NULL AUTO_INCREMENT,
-  department_name VARCHAR(30) NOT NULL,
-  PRIMARY KEY (id)
-);ro
+  first_name VARCHAR(30) NOT NULL,
+  last_name VARCHAR(30) NOT NULL,
+  role_id INT NOT NULL,
+  manager_id INT,
+  PRIMARY KEY (id),
+  FOREIGN KEY (role_id) REFERENCES employee_role(id) ON DELETE CASCADE,
+  FOREIGN KEY (manager_id) REFERENCES employee(id) ON DELETE SET NULL
+);
