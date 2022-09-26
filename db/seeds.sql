5578eb947fbcfe4c717157b3be9329f997bcb513
PatrickARatcliff <pratcliff5@gmail.com>
2022-09-25 20:08:03 -0600

    adjust foreign key on emp table

--- db/seeds.sql
+++ db/seeds.sql
@@ -1,5 +1,6 @@
 INSERT INTO employee (first_name, last_name, role_id, manager_id)
-VALUES ("John", "Doe", 1, null),
+VALUES 
+    ("John", "Doe", 1, null),
     ("Mike", "Chan", 2, 1),
     ("Ashley", "Rodriguez", 3, null),
     ("Kevin", "Tupik", 4, 3),
@@ -10,7 +11,8 @@ VALUES ("John", "Doe", 1, null),
        
 
 INSERT INTO employee_role (title, salary, department_id)
-VALUES ("Sales Lead", 100000, 1),
+VALUES 
+    ("Sales Lead", 100000, 1),
     ("Salesperson", 80000, 1),
     ("Lead Engineer", 150000, 2),
     ("Software Engineer", 120000, 2),
@@ -20,7 +22,8 @@ VALUES ("Sales Lead", 100000, 1),
     ("Lawyer", 100000, 4);
 
 INSERT INTO department (department_name)
-VALUES ("Sales"),
-       ("Engineering"),
-       ("Finance"),
-       ("Legal");
+VALUES 
+    ("Sales"),
+    ("Engineering"),
+    ("Finance"),
+    ("Legal");
